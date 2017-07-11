b943c2c7f8736c0cab9867b70af3c444d1d34c66
James Fogarty <jayfo.github@gmail.com>
2017-07-11 06:39:26 -0700

    Update PersonaFactory to Poll Server

--- familysleep/app/scripts/services/personafactory.js
+++ familysleep/app/scripts/services/personafactory.js
@@ -1,187 +1,119 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @id FamilySleep.personaFactory
- * @description
- * # personaFactory
- * Factory in the FamilySleep. Contains each family member's profile pic, target hours, id, and type of family member
- */
- /* format of profiles object
-{
-  "data": [{
-    "name": "Rob",
-    "targetedHours": 9,
-    "profilePic": "images/avatars/momcircle.png",
-    "relation": "parent1",
-    "id": "mom"
-  }, {
-    "name": "Pat",
-    "targetedHours": 8,
-    "profilePic": "images/avatars/dadcircle.png",
-    "relation": "parent2",
-    "id": "dad"
-  }, {
-    "name": "JR",
-    "targetedHours": 10,
-    "profilePic": "images/avatars/girlcircle.png",
-    "relation": "child1",
-    "id": "boy"
+// "personas": {
+//     "m1": {
+//         "name": "Rob",
+//         "targetedHours": 9,
+//         "profilePic": "images/avatars/momcircle.png",
+//         "relation": "parent",
+//         "pid": "m1",
+//         "fitbit": "4KBZLY"
+//     },
+//     "m2": {
+//         "name": "Pat",
+//         "targetedHours": 8,
+//         "profilePic": "images/avatars/dadcircle.png",
+//         "relation": "parent",
+//         "pid": "m2",
+//         "fitbit": "4KB123"
+//     },
+//     "m3": {
+//         "name": "JR",
+//         "targetedHours": 10,
+//         "profilePic": "images/avatars/girlcircle.png",
+//         "relation": "child",
+//         "pid": "m3",
+//         "fitbit": "4KATL6"
+//
+//     },
+//     "m4": {
+//         "name": "AJ",
+//         "targetedHours": 10,
+//         "profilePic": "images/avatars/boycircle.png",
+//         "relation": "child",
+//         "pid": "m4",
+//         "fitbit": "22TL6D"
+//     }
+// }
 
-  }, {
-    "name": "AJ",
-    "targetedHours": 10,
-    "profilePic": "images/avatars/boycircle.png",
-    "relation": "child2",
-    "id": "girl"
-  }]
-}
+var module = angular.module(
+    'FamilySleep'
+)
+    
+module.factory(
+    'personaFactory', 
+    [
+        '$rootScope', '$http', '$timeout', 'BASEURL_PYRAMID',
+        function ($rootScope, $http, $timeout, BASEURL_PYRAMID) {
+            var factory = {};
 
- */
-angular.module('FamilySleep')
-  .factory('personaFactory', ['$http', 'localStorageService', 'BASEURL_PYRAMID', function ($http, localStorageService, BASEURL_PYRAMID) {
-    // Service logic
-    // ...
-    //array holding all the profiles, structure is set above
-    var profiles = [];
+            //
+            // Our personas data from the server
+            //
+            factory.personas = null;
 
-    var temp_data; 
+            //
+            // Retrieve our personas data, schedule our next retrieve
+            //
+            factory._retrievePersonas = function () {
+                $http(
+                    {
+                        method: 'GET',
+                        url: BASEURL_PYRAMID + '/document/familysleep_personas'
+                    }
+                ).then(function (response) {
+                    factory.personas = response.data.personas;
+                    factory._notify();
+                }).catch(function () {
 
-    var retrieveProfiles = function() {
-      //get file for profiles and update them 
-      //console.log("at persona Factory");
-        return $http(
-            {
-                method: 'GET',
-                url: BASEURL_PYRAMID + '/document/familysleep_personas'
+                }).finally(function () {
+                    factory._scheduleNextRetrieve();
+                });
+            };
+
+            //
+            // Track who is listening to us, start/stop retrieval
+            //
+            factory._numberObservers = 0;
+            factory._nextRetrievePromise = null;
+
+            factory.observe = function (scope, callback) {
+                var deregister = $rootScope.$on('personaFactory-personas', callback);
+
+                factory._numberObservers += 1;
+                factory._retrievePersonas();
+
+                scope.$on('$destroy', function () {
+                    deregister();
+
+                    factory._numberObservers -= 1;
+                    if (factory._numberObservers == 0) {
+                        $timeout.cancel(factory._nextRetrievePromise);
+                        factory._nextRetrievePromise = null;
+                    }
+                });
             }
-        )
-        .then(function (response) {
-          // this callback will be called asynchronously
-          // when the response is available
-          temp_data = response.data;
-          //console.log(temp_data);
-          populate(temp_data);
-          //console.log(temp_data);
 
-          if(localStorageService.isSupported) {
-            console.log('set');
-            var result = localStorageService.set('auth', temp_data);
-            if(result) {
-              console.log('setting worked!-------------------------');
+            factory._notify = function () {
+                $rootScope.$emit('personaFactory-personas');
             }
-            var value = localStorageService.get('auth');
-            if(value != null) {
-              console.log('get worked! -------');
-              console.log("printing temp_data");
-              console.log(value);
-            }
-          }
 
+            //
+            // Schedule another retrieve, if anybody is listening
+            //
+            factory._scheduleNextRetrieve = function () {
+                $timeout.cancel(factory._nextRetrievePromise);
+                if (factory._numberObservers > 0) {
+                    factory._nextRetrievePromise = $timeout(factory._retrievePersonas, 30 * 1000);
+                }
+            };
 
+            //
+            // Initial retrieval
+            //
+            factory._retrievePersonas();
 
-          //populate(temp_data);
-        }, function (response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-          console.error('Error' + response.statusText);
-        });
-    }
-
-    var populate = function(temp_data) {
-      profiles = temp_data.personas;
-      /*console.log("in populate part of the personaFactory");
-      console.log(profiles);*/  
-    }
-
-
-    var getProfile = function (id) {
-      for(var i = 0; i < profiles.length; i++) { 
-        if(profiles[i].id == id) {
-          return profiles[i];
+            return factory;
         }
-      }
-    }
-
-    var getAllProfiles = function() {
-      return profiles;
-    }
-
-    var getAllIDs = function(){
-      var IDArray = new Array(profiles.length);
-      for(var i = 0; i < profiles.length; i++) {
-          IDArray[i] = profiles[i].id;
-      }
-      return IDArray;
-    }
-
-    var getProfPic = function(id) {
-      for(var i = 0; i < profiles.length; i++) {
-        if(profiles[i].id == id) {
-          return profiles[i].profilePic;
-        }
-      }
-    }
-
-    var getTargetedHours = function(id) {
-      for(var i = 0; i < profiles.length; i++) {
-        if(profiles[i].id == id) {
-          return profiles[i].targetedHours;
-        }
-      }
-    }
-
-    var getTargetedHours = function(id) {
-      for(var i = 0; i < profiles.length; i++) {
-        if(profiles[i].id == id) {
-          return profiles[i].targetedHours;
-        }
-      }
-    }
-
-    var getRelation = function(id) {
-      for(var i = 0; i < profiles.length; i++) {
-        if(profiles[i].id == id) {
-          return profiles[i].relation;
-        }
-      }
-    }
-
-    var getName = function(id) {
-      for(var i = 0; i < profiles.length; i++) {
-        console.log(profiles[i].relation);
-        if(profiles[i].relation == id) {
-          return profiles[i].name;
-        }
-      }
-    }
-
-    var getAllNames = function(){
-      var nameArray = new Array(profiles.length);
-      for(var i = 0; i < profiles.length; i++) {
-          //console.log(profiles[i].name);
-          nameArray[i] = profiles[i].name;
-      }
-      //console.log("getAllNames Function");
-      //console.log(nameArray);
-      return nameArray;
-    }
-
-    //calling retrieveProfiles(); from here needs to go somewhere else
-
-    retrieveProfiles();
-    //console.log(profiles);
-    // Public API here
-    return {
-      getProfPic: getProfPic,
-      getTargetedHours: getTargetedHours,
-      getRelation : getRelation,
-      getAllProfiles : getAllProfiles,
-      getProfile: getProfile,
-      retrieveProfiles: retrieveProfiles,
-      getName: getName,
-      getAllNames: getAllNames,
-      getAllIDs: getAllIDs
-
-    };
-  }]);
+    ]
+);
