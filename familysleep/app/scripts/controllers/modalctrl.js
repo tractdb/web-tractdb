8b0a0901b24c2f872800d3df1809fafe01b872c9
lrpina <pina.laura@gmail.com>
2017-11-07 18:13:30 -0800

    fixed the missing name on the modal!

--- familysleep/app/scripts/controllers/modalctrl.js
+++ familysleep/app/scripts/controllers/modalctrl.js
@@ -39,9 +39,11 @@ angular.module('FamilySleep').controller('ModalCrtl', ['selfReportState', '$uibM
 
    
 	$ctrl.open = function (famID) {
-		$log.info("in open of ModalCrtl"); //added this might need to pass log
+		//$log.info("in open of ModalCrtl"); //added this might need to pass log
     var date = dateFactory.getDateString();
-     //$ctrl.famMems = personaFactory.getAllNameIDs();
+    //console.log(famID);
+    $ctrl.famID = famID;
+
     $ctrl.famMems = personaFactory.getAllNames();
     //console.log($ctrl.famMems);
     $ctrl.famIDs = personaFactory.getAllIDs();
@@ -51,9 +53,8 @@ angular.module('FamilySleep').controller('ModalCrtl', ['selfReportState', '$uibM
     /*console.log("printing $ctrl.states object from ModalCrtl");
     console.log($ctrl.states);*/
     //$ctrl.famID;
+    $ctrl.famName = personaFactory.getName(famID);
 
-    console.log(famID);
-    $ctrl.famID = famID;
     $ctrl.famButton = false;
     $ctrl.moodButton = false;
 
@@ -63,6 +64,7 @@ angular.module('FamilySleep').controller('ModalCrtl', ['selfReportState', '$uibM
             break;
         }
     }
+
     var modalInstance = $uibModal.open({
       animation: $ctrl.animationsEnabled,
       ariaLabelledBy: 'modal-title',
@@ -82,6 +84,9 @@ angular.module('FamilySleep').controller('ModalCrtl', ['selfReportState', '$uibM
         },
         famID: function(){
           return $ctrl.famID;
+        },
+        famName: function(){
+          return $ctrl.famName;
         }
       }
     });
@@ -132,10 +137,11 @@ angular.module('FamilySleep').controller('ModalCrtl', ['selfReportState', '$uibM
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 
-angular.module('FamilySleep').controller('ModalInstanceCtrl', function ($uibModalInstance, items, famMems, $log, famID) {
+angular.module('FamilySleep').controller('ModalInstanceCtrl', function ($uibModalInstance, items, famMems, $log, famID, famName) {
   var $ctrl = this;
   $ctrl.items = items;
-  //$ctrl.famID = famID; //not sure if we need this
+  $ctrl.famID = famID; //not sure if we need this
+  $ctrl.famName = famName;
   //creating an object
   /*$ctrl.selected = {
     item: $ctrl.items[0]
